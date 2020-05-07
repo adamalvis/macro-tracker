643a0b9c77adfab1f0ea314c3d6a32722ab4e058
Adam Alvis <adam.alvis@gmail.com>
2020-05-07 13:53:00 -0400

    Updates

---
+++ precache-manifest.56485156a3047004a4aeac041366916d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293fa25ac693562ca4a93154c7689170",
+    "url": "/macro-tracker/index.html"
+  },
+  {
+    "revision": "a24dd17280285c699b5f",
+    "url": "/macro-tracker/static/css/2.fe8af591.chunk.css"
+  },
+  {
+    "revision": "f107339c3f7aff806bda",
+    "url": "/macro-tracker/static/css/main.3707d473.chunk.css"
+  },
+  {
+    "revision": "a24dd17280285c699b5f",
+    "url": "/macro-tracker/static/js/2.30b08f79.chunk.js"
+  },
+  {
+    "revision": "fbd95cba4fc295db828667b3d98ce35f",
+    "url": "/macro-tracker/static/js/2.30b08f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f107339c3f7aff806bda",
+    "url": "/macro-tracker/static/js/main.b795b1ba.chunk.js"
+  },
+  {
+    "revision": "5f23df4539edf52ced00",
+    "url": "/macro-tracker/static/js/runtime-main.2d32e78f.js"
+  }
+]);
