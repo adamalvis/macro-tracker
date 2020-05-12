96ce5f324805ea948f3e35dd67a75b7b79484edc
Adam Alvis <adam.alvis@gmail.com>
2020-05-11 20:43:57 -0400

    Updates

--- precache-manifest.65fdd4e8174db7937f5fedc98fa64b56.js
+++ precache-manifest.65fdd4e8174db7937f5fedc98fa64b56.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab02362042bdbcbea08306fedf65d84a",
+    "revision": "086acc5c3d087e3aa23e0eb4e1bb19a7",
     "url": "https://adamalvis.github.io/macro-tracker/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://adamalvis.github.io/macro-tracker/static/css/2.fe8af591.chunk.css"
   },
   {
-    "revision": "1ee981c2f68a1408d088",
+    "revision": "93de65486c5cacd5fe3b",
     "url": "https://adamalvis.github.io/macro-tracker/static/css/main.3707d473.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://adamalvis.github.io/macro-tracker/static/js/2.30b08f79.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1ee981c2f68a1408d088",
-    "url": "https://adamalvis.github.io/macro-tracker/static/js/main.a5507c4e.chunk.js"
+    "revision": "93de65486c5cacd5fe3b",
+    "url": "https://adamalvis.github.io/macro-tracker/static/js/main.2a1b500b.chunk.js"
   },
   {
     "revision": "692cab4dd57045f9741b",
